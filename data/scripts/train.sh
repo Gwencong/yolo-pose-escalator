5550b7127be818bdea1f60815805f6b99803c196
Gwencong <gwc3667@gmail.com>
2022-09-24 19:35:06 +0800

    add train.sh

---
+++ data/scripts/train.sh
@@ -0,0 +1,45 @@
+# finetune (custom data + coco data)
+python train.py \
+    --weights weights/yolov5l6_pose.pt \
+    --cfg models/hub/yolov5l6_kpts.yaml \
+    --data data/merge_kpts.yaml \
+    --hyp data/hyp.finetune_custom.yaml \
+    --epochs 200 \
+    --batch-size 64 \
+    --img-size 832 \
+    --device 0 \
+    --workers 8 \
+    --kpt-label \
+    --freeze 12 \
+    --project runs/finetune
+    
+
+# finetune (custom data only)
+python train.py \
+    --weights weights/yolov5l6_pose.pt \
+    --cfg models/hub/yolov5l6_kpts.yaml \
+    --data data/custom_kpts.yaml \
+    --hyp data/hyp.finetune_custom.yaml \
+    --epochs 200 \
+    --batch-size 64 \
+    --img-size 832 \
+    --device 0 \
+    --workers 8 \
+    --kpt-label \
+    --freeze 12 \
+    --project runs/finetune
+    
+
+# scratch (custom data only)
+python train.py \
+    --weights weights/yolov5l6_pose.pt \
+    --cfg models/hub/yolov5l6_kpts.yaml \
+    --data data/custom_kpts.yaml \
+    --hyp data/hyp.scratch.yaml \
+    --epochs 300 \
+    --batch-size 64 \
+    --img-size 832 \
+    --device 0 \
+    --workers 8 \
+    --kpt-label \
+    --project runs/train
